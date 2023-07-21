a9aae721f162869e063cab25454beff1cb73ddcb
Vignesh <vighneshgupta32@gmail.com>
2023-07-21 21:06:20 +0530

    added form

--- app/survey/page.tsx
+++ app/survey/page.tsx
@@ -1,14 +1,15 @@
-'use client'
+"use client";
 
+import SurveyForm from "@/components/SurveyForm";
 import React from "react";
 
 const SurveyPage = () => {
   return (
-    <div className="flex flex-col items-center gap-3 p-10 text-center">
+    <div className="flex flex-col items-center gap-3 p-10 ">
       <h1 className="text-2xl font-bold text-white">
         Welcome to HCL&apos;s Cultural Data Platform
       </h1>
-      <p className="max-w-3xl text-white md:text-lg">
+      <p className="max-w-5xl text-center text-white md:text-lg">
         This is an organizational survey that is asking some questions regarding
         your organizational environment. It is asking you to consider your
         colleagues, team members, leaders and senior leadership around you when
@@ -20,10 +21,11 @@ const SurveyPage = () => {
         in the beginning there are some general questions regarding you, your
         work location and role.
       </p>
-      <div className="w-full py-8 mt-10 text-center bg-white rounded-lg md:w-3/4">
-        <p className="font-semibold text-primary">
+      <div className="items-center w-full py-8 mt-10 bg-white rounded-lg md:w-3/4">
+        <p className="mb-3 font-semibold text-center text-primary">
           Please submit the form below to the best of your knowledge
         </p>
+        <SurveyForm />
       </div>
     </div>
   );
--- src/components/SurveyForm.tsx
+++ src/components/SurveyForm.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
 
 const SurveyForm = () => {
-  return (
-    <div>SurveyForm</div>
-  )
-}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
 
-export default SurveyForm
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <div className="mx-16">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>What is the name of your Organization?</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default SurveyForm;
